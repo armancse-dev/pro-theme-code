8e39a4b08c0295d2325fbb46b3f9ac460ae00833
Arman Hossain <armanhossaincse7@gmail.com>
2023-07-18 20:14:15 +0600

    page tempalte

---
+++ page_full_width.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Full width Page
+ */
+    get_header();
+?>
+    <section id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <<?php if (have_posts()) :
+              while (have_posts()) : the_post(); ?>
+
+              <div class="blog_area">
+                  <div class="post_thumb">
+                    <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                  </div>
+                  <div class="post_details">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p><i class="fas fa-calendar-alt"></i> <?php echo the_time('D, j, F Y'); ?><span>At</span> <i class="fas fa-clock"></i><?php echo the_time('g:i a'); ?></p>
+                    <?php the_excerpt(); ?>
+                  </div>
+                  
+              </div>
+          <?php endwhile;
+            else :
+              _e('No post found');
+            endif; ?>
+          <div id="page_nav">
+            <?php if ('ali_pagenav') {ali_pagenav(); } else{ ?>
+                <?php next_posts_link(); ?>
+                <?php previous_posts_link(); ?>
+            <?php } ?>
+          </div>
+                   
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
