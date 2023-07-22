ea4a5717de612a3cc0bb2f0a597410d2d12eb220
Arman Hossain <armanhossaincse7@gmail.com>
2023-07-22 15:53:13 +0600

    custom theme option sub menu

--- inc/theme-option/admin_theme_option.php
+++ inc/theme-option/admin_theme_option.php
@@ -11,11 +11,27 @@ function an_add_theme_page(){
 
    // );
    add_menu_page('Theme Option for Admin','Theme Option','manage_options','an_theme_option','an_theme_create_page', 'dashicons-visibility',10);
+
+   add_submenu_page( 'an_theme_option', 'Theme Option', 'General', 'manage_options', 'an_theme_option', 'an_theme_create_page' );
+
+   add_submenu_page( 'an_theme_option', 'Theme Custom CSS', 'Custom Css', 'manage_options', 'an_custom_css', 'an_theme_custom_css_page' );
+
+   add_submenu_page( 'an_theme_option', 'Theme Custom JS', 'Custom JS', 'manage_options', 'an_custom_js', 'an_theme_custom_js_page' );
 }
 add_action('admin_menu', 'an_add_theme_page');
 
 function an_theme_create_page(){
    //generating theme option
-   echo "<h1>Theme Option</h1>";
-   bloginfo( 'name' );
+   require_once( get_template_directory(). '/inc/theme-option/general.php' );
+}
+
+function an_theme_custom_css_page(){
+   // Generating 
+   require_once( get_template_directory(). '/inc/theme-option/custom_css.php' );
+}
+
+function an_theme_custom_js_page(){
+   // Generating theme option
+  
+   require_once( get_template_directory(). '/inc/theme-option/custom_js.php' );
 }
---
+++ inc/theme-option/custom_css.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h1>Theme Common Options</h1>";
